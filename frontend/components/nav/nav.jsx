ef8b78fcd7d81fc46d6adcea11e54797bf128485
Maya Rosmarin <maya.rosmarin@gmail.com>
2018-05-10 10:13:19 -0400

    Add debounce on searchbar

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -5,10 +5,12 @@ import GreetingContainer from '../greeting/greeting_container';
 import FaUser from 'react-icons/lib/fa/user';
 import FaSignout from 'react-icons/lib/fa/sign-out';
 import FaHome from 'react-icons/lib/fa/home';
+import FaVolumeUp from 'react-icons/lib/fa/volume-up';
 
 export default (props) => {
   return (
     <ul className="nav-bar">
+      <FaVolumeUp />
       <li><Link className="search-link" to="search"><FaSearch />     Search</Link>
       <br />
       <br />
--- frontend/components/search/search_form.jsx
+++ frontend/components/search/search_form.jsx
@@ -12,26 +12,16 @@ class SearchForm extends React.Component {
   }
 
   handleChange (e) {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
     this.setState(
-      { searchQuery: e.target.value },
-      () => this.props.requestAllPodcasts(this.state.searchQuery)
-    )
-  }
-
-  debounce(func, wait, immediate) {
-  	var timeout;
-  	return function() {
-  		var context = this, args = arguments;
-  		var later = function() {
-  			timeout = null;
-  			if (!immediate) func.apply(context, args);
-  		};
-  		var callNow = immediate && !timeout;
-  		clearTimeout(timeout);
-  		timeout = setTimeout(later, wait);
-  		if (callNow) func.apply(context, args);
-  	};
-  };
+    { searchQuery: e.target.value },
+    () => {
+      this.timeout = setTimeout(() => this.props.requestAllPodcasts(this.state.searchQuery), 500)
+    }
+  )
+};
 
   render () {
 
